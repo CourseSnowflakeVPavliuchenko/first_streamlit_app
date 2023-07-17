6e18ade3a536fd667c89156c2d0d5ea4f71b3f85
CourseSnowflakeVPavliuchenko <139747849+CourseSnowflakeVPavliuchenko@users.noreply.github.com>
2023-07-17 10:12:45 +0200

    Update streamlit_app.py
    add Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfest Menu')
-streamlit.text('Omega 3 & Bluebarry Ostmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('🥣Omega 3 & Bluebarry Ostmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Hard-Boiled Free Range Egg')
